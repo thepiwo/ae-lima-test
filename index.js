1a48a6e9189fc920ab1c61a16d4d4d45b0c5fe8b
Philipp Piwo <github@piwo.space>
2020-01-23 09:37:27 +0100

    update sdk test

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const Universal = require("@aeternity/aepp-sdk").Universal;
+const {Universal, Node, MemoryAccount} = require("@aeternity/aepp-sdk");
 
 const main = async () => {
     const client = await Universal({
-        url: "http://localhost:3013",
-        internalUrl: "http://localhost:3113",
-        networkId: "ae_next",
-        keypair: {
-            publicKey: "ak_2VnwoJPQgrXvreUx2L9BVvd9BidWwpu1ASKK1AMre21soEgpRT",
-            secretKey: ""
-        }
+        nodes: [{
+            name: 'testnet',
+            instance: await Node({
+                url: "https://sdk-testnet.aepps.com",
+                internalUrl: "https://sdk-testnet.aepps.com"
+            })
+        }],
+        compilerUrl: "https://compiler.aepps.com",
+        accounts: [MemoryAccount({
+            keypair: {
+                publicKey: "ak_2VnwoJPQgrXvreUx2L9BVvd9BidWwpu1ASKK1AMre21soEgpRT",
+                secretKey: ""
+            }
+        })]
     });
 
-    console.log(client);
+    const contractSouce = `
+@compiler >= 4
+
+contract Example =
+  type state = int
+  entrypoint init() = 0
+  stateful entrypoint set(x : int) = put(x)
+  entrypoint get() = state`;
+
+    const contract = await client.getContractInstance(contractSouce);
+    await contract.deploy();
+
+    const nonce = (await client.getAccount("ak_2VnwoJPQgrXvreUx2L9BVvd9BidWwpu1ASKK1AMre21soEgpRT")).nonce;
+
+    [...Array(10).keys()].reduce(async (acc, i) => {
+        await acc;
+        const nextNonce = nonce + 1 + i;
+        console.log(nonce, nextNonce);
+
+        await contract.methods.set(i, {nonce: nextNonce, waitMined: false});
+    }, Promise.resolve());
+
 };
 
 main();
